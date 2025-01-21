58931d330fa20a63adef226251c138d31b1aa531
tacascer <trandangtrithanh2000@gmail.com>
2025-01-20 23:05:43 -0500

    feat: can end a game

--- guess-the-word/src/test/kotlin/com/github/lowkeylab/guesstheword/game/GameServiceTest.kt
+++ guess-the-word/src/test/kotlin/com/github/lowkeylab/guesstheword/game/GameServiceTest.kt
@@ -2,7 +2,6 @@ package com.github.lowkeylab.guesstheword.game
 
 import com.github.lowkeylab.guesstheword.TestContainersConfig
 import io.kotest.matchers.nulls.shouldNotBeNull
-import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -39,45 +38,5 @@ class GameServiceTest {
         val output = sut.get(savedGame.id!!)
 
         output.shouldNotBeNull()
-    }
-
-    @Test
-    fun `can add a player to a game`() {
-        val sut = GameService(gameRepository)
-        val game = sut.new()
-        val player = Player("Alice")
-
-        sut.addPlayer(game, player)
-
-        game.numberOfPlayers shouldBe 1
-    }
-
-    @Test
-    fun `can start a game`() {
-        val sut = GameService(gameRepository)
-        val game = sut.new()
-        val alice = Player("Alice")
-        val bob = Player("Bob")
-        sut.addPlayer(game, alice)
-        sut.addPlayer(game, bob)
-
-        sut.start(game)
-
-        game.currentRound shouldBe 1
-    }
-
-    @Test
-    fun `can add a guess to a game`() {
-        val sut = GameService(gameRepository)
-        val game = sut.new()
-        val alice = Player("Alice")
-        val bob = Player("Bob")
-        sut.addPlayer(game, alice)
-        sut.addPlayer(game, bob)
-        sut.start(game)
-
-        sut.addGuess(game, alice, "apple")
-
-        game.guessesForRound(1)[alice] shouldBe "apple"
     }
 }
