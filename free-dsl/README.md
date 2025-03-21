749880027ebd2c29d51b1fd56b3d53cb857aecf0
Tim Tran <49887921+tacascer@users.noreply.github.com>
2025-03-21 00:29:03 -0400

    build: set up free-dsl project (#129)
    * ```
    feat: add FreeDSL module to the project
    
    Introduced a new "free-dsl" module inspired by FreeBuilder to provide automatic builders for Kotlin data classes. This includes setup files such as `build.gradle.kts`, `settings.gradle.kts`, and a `README.md` file explaining its purpose. The module follows multiplatform conventions and integrates with the project's monorepo structure.
    ```
    
    * feat(build): set group and version for free-dsl module
    
    Added group and initial version to free-dsl build script. Configured release-please for free-dsl with a "go" release type and extra files support.
    
    * feat(linting): add ktlint support for Kotlin Gradle scripts
    
    Added ktlint configuration for Kotlin Gradle scripts to enforce coding standards. This ensures consistent code style in both Kotlin source and Gradle files.
    
    * chore: fix linting issues

---
+++ free-dsl/README.md
@@ -0,0 +1,4 @@
+# FreeDSL
+
+Inspired by [FreeBuilder](https://github.com/inferred/FreeBuilder), FreeDsl aims
+to provide automatic builders for Kotlin data classes.
---
+++ free-dsl/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("kotlin-multiplatform-conventions")
+}
+
+group = "com.github.lowkeylab"
+version = "0.0.1" // x-release-please-version
+
+kotlin {
+    jvm()
+    sourceSets {
+        commonMain {
+        }
+    }
+}
