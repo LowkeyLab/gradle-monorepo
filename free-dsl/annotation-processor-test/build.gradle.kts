7879ba4d4740ac90cc4da3878a520af540f4795b
tacascer <trandangtrithanh2000@gmail.com>
2025-03-28 17:53:39 -0400

    feat(build): remove custom KSP generated code output config
    The custom KSP configuration for outputting generated code has been removed. This simplifies the build file and relies on default KSP behavior for managing generated code.

--- free-dsl/annotation-processor-test/build.gradle.kts
+++ free-dsl/annotation-processor-test/build.gradle.kts
@@ -27,9 +27,3 @@ kotlin {
 dependencies {
     add("kspJvm", project(":annotation-processor"))
 }
-
-// Configure KSP to output generated code to a git-trackable directory
-ksp {
-    // Use the absolute path to ensure KSP can find the directory
-    arg("ksp.kotlin.generated", file("${project.projectDir}/src/generated/kotlin").absolutePath)
-}
