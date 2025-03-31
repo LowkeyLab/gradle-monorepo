3369b0f6c7bc416db6b22c755b5f8a1c1eb1551f
tacascer <trandangtrithanh2000@gmail.com>
2025-03-31 08:09:42 -0400

    feat(build): integrate Kover and SonarQube for code coverage
    Replaced SonarQube integration in `annotation-processor-test` module with Kover and added both tools to other modules. Simplified task dependencies and updated project configuration to enhance code coverage and analysis workflow.

--- free-dsl/annotation-processor-test/build.gradle.kts
+++ free-dsl/annotation-processor-test/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("kotlin-multiplatform-conventions")
     alias(libs.plugins.google.ksp)
-    alias(libs.plugins.sonarqube)
+    id(
+        libs.plugins.kotlinx.kover
+            .get()
+            .pluginId,
+    )
 }
 
 group = rootProject.group
@@ -21,27 +25,4 @@ kotlin {
 // Add the annotation processor as a KSP dependency
 dependencies {
     add("kspJvm", project(":annotation-processor"))
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "lowkeylab_gradle-monorepo_free-dsl")
-        property("sonar.organization", "lowkeylab")
-        property(
-            "sonar.coverage.jacoco.xmlReportPaths",
-            "${layout.buildDirectory.asFile.get()}/reports/kover/report.xml",
-        )
-        property(
-            "sonar.userHome",
-            "${layout.buildDirectory.asFile.get()}/.sonar",
-        )
-    }
-}
-
-tasks.sonar {
-    dependsOn(tasks.koverXmlReport)
-}
-
-tasks.checkCI {
-    dependsOn(tasks.sonar)
 }
--- free-dsl/annotation-processor/build.gradle.kts
+++ free-dsl/annotation-processor/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("kotlin-multiplatform-conventions")
-    alias(libs.plugins.google.ksp) apply false
+    id(
+        libs.plugins.kotlinx.kover
+            .get()
+            .pluginId,
+    )
 }
 
 group = rootProject.group
