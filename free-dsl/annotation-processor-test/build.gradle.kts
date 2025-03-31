12f8f1fac4c13ee6850e6b4b41058d77e70c2921
tacascer <trandangtrithanh2000@gmail.com>
2025-03-31 07:58:28 -0400

    feat: add SonarQube plugin and configuration for code analysis
    Introduced the SonarQube plugin to enhance code quality monitoring. Configured project-specific properties and tasks to integrate with Sonar. Updated task dependencies to include Sonar as part of the CI workflow.

--- free-dsl/annotation-processor-test/build.gradle.kts
+++ free-dsl/annotation-processor-test/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("kotlin-multiplatform-conventions")
     alias(libs.plugins.google.ksp)
+    alias(libs.plugins.sonarqube)
 }
 
 group = rootProject.group
@@ -20,4 +21,27 @@ kotlin {
 // Add the annotation processor as a KSP dependency
 dependencies {
     add("kspJvm", project(":annotation-processor"))
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "lowkeylab_gradle-monorepo_free-dsl")
+        property("sonar.organization", "lowkeylab")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.asFile.get()}/reports/kover/report.xml",
+        )
+        property(
+            "sonar.userHome",
+            "${layout.buildDirectory.asFile.get()}/.sonar",
+        )
+    }
+}
+
+tasks.sonar {
+    dependsOn(tasks.koverXmlReport)
+}
+
+tasks.checkCI {
+    dependsOn(tasks.sonar)
 }
