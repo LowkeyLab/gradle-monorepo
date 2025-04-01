e35e6684caff1a31f2e129b540b836986308a04c
tacascer <trandangtrithanh2000@gmail.com>
2025-03-31 21:13:32 -0400

    fix(build): update group ID and configure Maven publishing
    Updated the group ID to follow standardized naming conventions and added Maven publishing configurations for the annotation processor module. Integrated Sonatype Central support, licensing, and developer metadata to enable proper artifact distribution.

--- free-dsl/annotation-processor/build.gradle.kts
+++ free-dsl/annotation-processor/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("kotlin-multiplatform-conventions")
     id(
@@ -5,6 +7,7 @@ plugins {
             .get()
             .pluginId,
     )
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 group = rootProject.group
@@ -23,6 +26,34 @@ kotlin {
             dependencies {
                 implementation(libs.kotest.assertions.core)
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    pom {
+        name.set("Free-DSL Annotation Processor")
+        description.set("Kotlin Symbol Processor for generating idiomatic Kotlin DSL builders for data classes and regular classes with primary constructors.")
+        url = "https://github.com/LowkeyLab/gradle-monorepo"
+        licenses {
+            license {
+                name = "Affero GPL v3.0"
+                url = "https://opensource.org/licenses/AGPL-3.0"
+            }
+        }
+        developers {
+            developer {
+                id.set("tacascer")
+                name.set("Tim Tran")
+                url.set("https://github.com/tacascer")
+            }
+        }
+        scm {
+            url.set("https://github.com/LowkeyLab/gradle-monorepo")
+            connection.set("scm:git:git@github.com:LowkeyLab/gradle-monorepo.git")
+            developerConnection.set("scm:git:git@github.com:LowkeyLab/gradle-monorepo.git")
         }
     }
 }
