53e4f13a4d31e792e9208465099370c89de866d2
Tim Tran <49887921+tacascer@users.noreply.github.com>
2025-04-01 10:32:37 -0400

    chore: release main

--- guess-the-word/build.gradle.kts
+++ guess-the-word/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.lowkeylab"
-version = "1.1.0" // x-release-please-version
+version = "1.2.0" // x-release-please-version
 
 dependencies {
     implementation(libs.spring.boot.starterDataMongodb)
