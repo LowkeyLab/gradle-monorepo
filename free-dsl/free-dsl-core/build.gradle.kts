a119225f88c2c405745f6caf7d3ad7a183d97a74
tacascer <trandangtrithanh2000@gmail.com>
2025-08-18 22:46:52 +0100

    build: remove deprecated SonatypeHost usage in Maven publishing

--- free-dsl/free-dsl-core/build.gradle.kts
+++ free-dsl/free-dsl-core/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     id("kotlin-multiplatform-conventions")
     id(
@@ -31,7 +29,7 @@ kotlin {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
     signAllPublications()
     pom {
         name.set("Free-DSL")
